ddb5b4d2cb58a51a6955e8587474fa42ae96e49a
Nikita Miroshnichenko <61200485+NikitaMirosha@users.noreply.github.com>
2020-03-25 23:26:23 +0300

    Update Spot.java

--- src/Spot.java
+++ src/Spot.java
@@ -1,12 +1,13 @@
-package com.mirosha.game;
+package mirosha.game;
 
-public class Spot {
-	
-	public int row;
-	public int col;
+public class Spot { // класс для размещения срок и столбцов по координатам
+
+	public int row; // строка
+	public int col; // столбец 
 	
 	public Spot(int row, int col) {
-		this.row = row;
-		this.col = col;
+		this.row = row; // явный указатель на строку
+		this.col = col; // явный указатель на столбец
 	}
 }
+
